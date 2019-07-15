5c45cfcc082c193f4f0a1d8728d3dd3f99fd84d1
Viet Anh Phan <phanvietanh90@gmail.com>
2019-07-15 00:04:21 -0500

    Create Post and get Timeline

--- frontend/assets/js/initservice.js
+++ frontend/assets/js/initservice.js
@@ -1,9 +1,35 @@
 $(function () {
     let CREATE_POST_URL = "http://localhost:8080/api/post";
+    let GET_TIMELINE_URL = "http://localhost:8080/api/timeline/bao@mum.edu";
     console.log("Test1.......");
+
+    // fetch post
+    $.ajax({
+        url: GET_TIMELINE_URL,
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            Handlebars.registerHelper('fromNow', function(date) {
+                if (moment) {
+                    return moment(date).fromNow();
+                } else {
+                    return date;
+                }
+            });
+
+            Handlebars.registerHelper('numComments', function(comments) {
+                return comments.length;
+            });
+
+            var template = $('#handlebar').html();
+            var templateScript = Handlebars.compile(template);
+            var html = templateScript(data);
+            $('#posts').append(html);
+        }
+    });
+
     // Create post
     $('#post').click(function () {
-        console.log("Test2.......");
         let content = $('#exampleTextarea').val();
         let data = {
             "postId": "73367f9e3c1af178e3386f9eee24d9e4",
@@ -33,12 +59,14 @@ $(function () {
             contentType: 'application/json',
             data: JSON.stringify(data),
             success: function( data, textStatus ){
-                $('#response pre').html( data );
+                console.log("post sent");
             },
             error: function( jqXhr, textStatus, errorThrown ){
                 console.log( errorThrown );
             }
         });
-        console.log("post sent");
+        location.reload();
+
+
     });
 })
